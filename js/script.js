899cf1fb1804ae47cef3c7f119b4e1b0e10e552a
yar_otskyi <sasha236757@gmail.com>
2026-05-15 20:02:48 +0300

    First upload

---
+++ js/script.js
@@ -0,0 +1,88 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+// const number = prompt("Введіть число");
+
+// if (number > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// const value = prompt("Введіть значення");
+
+// value === "test" ? console.log(true) : console.log(false);
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// const number = Number(prompt("Введіть число"));
+
+// if (number > 10) {
+//   console.log(number - 5);
+// } else {
+//   console.log(number + 5);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const number = Number(prompt("Введіть число від 1 до 12"));
+
+// switch (number) {
+//   case 1:
+//     console.log("Січень");
+//     break;
+//   case 2:
+//     console.log("Лютий");
+//     break;
+//   case 3:
+//     console.log("Березень");
+//     break;
+//   case 4:
+//     console.log("Квітень");
+//     break;
+//   case 5:
+//     console.log("Травень");
+//     break;
+//   case 6:
+//     console.log("Червень");
+//     break;
+//   case 7:
+//     console.log("Липень");
+//     break;
+//   case 8:
+//     console.log("Серпень");
+//     break;
+//   case 9:
+//     console.log("Вересень");
+//     break;
+//   case 10:
+//     console.log("Жовтень");
+//     break;
+//   case 11:
+//     console.log("Листопад");
+//     break;
+//   case 12:
+//     console.log("Грудень");
+//     break;
+//   default:
+//     console.log("Введіть число від 1 до 12");
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// const number = prompt("Введіть тризначне число");
+
+// const a = Number(number[0]);
+// const b = Number(number[1]);
+// const c = Number(number[2]);
+
+// const sum = a + b + c;
+
+// console.log(sum);
